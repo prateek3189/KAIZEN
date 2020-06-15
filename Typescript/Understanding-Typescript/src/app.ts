872289da70163a57d862efa47fc105a3764f120a
prateek-tavisca <pmagarde@tavisca.com>
2020-06-15 23:02:21 +0530

    Removing Typescript from Here

--- Typescript/Understanding-Typescript/src/app.ts
+++
@@ -1,9 +0,0 @@
-// const button = document.querySelector("button")! as HTMLInputElement;
-// button.addEventListener("click",() => {
-//   console.log("Clicked");  
-// }); 
- 
-// const map = new Map();
-
-let a;
-a = 1 + 3;
